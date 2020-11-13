135278d2d30fb7da134ae7240bf4cdce83fb2faf
Diego-Nascimento <diegostealth3@gmail.com>
2020-11-13 08:29:27 -0300

    Background Intro

--- src/Components/IntroBackground/IntroBackground.js
+++ src/Components/IntroBackground/IntroBackground.js
@@ -3,24 +3,22 @@ import { Container, Star } from './IntroBackground.styles';
 
 function IntroBackground() {
   const [Stars, setStars] = React.useState([]);
+  const [AmountStars, setAmountStars] = React.useState(200);
 
   React.useEffect(() => {
     CreateStars();
   }, []);
 
   function CreateStars() {
-    for (let i = 0; i < 50; i++) {
-      const x = Math.floor(Math.random() * window.innerWidth);
-      const duration = Math.random() * (3 - 1 + 1) + 1;
-      const height = Math.random() * 1;
-      setStars([
-        ...Stars,
-        <Star Size={height} Left={x} Duration={duration} key={Math.random()} />,
-      ]);
+    if (Stars.length < AmountStars) {
+      for (let i = 0; i < AmountStars; i++) {
+        const x = Math.floor(Math.random() * window.innerWidth);
+        const duration = Math.random() * (3 - 1 + 1) + 1;
+        const height = Math.random() * 6;
+        setStars([...Stars, <Star Size={height} Left={x} Duration={duration} key={Math.random()} />]);
+      }
     }
   }
-
-  console.log(Stars);
 
   return <Container>{Stars.map((Star) => Star)}</Container>;
 }
