fb5557700b871ec7e11e8e8a3c346e26aefd89d9
Diego-Nascimento <diegostealth3@gmail.com>
2020-11-16 14:09:26 -0300

    ta indo a sobre

--- src/Pages/QuemSou/QuemSou.js
+++ src/Pages/QuemSou/QuemSou.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import * as PageActions from '../../store/modules/Page/actions';
 import { connect } from 'react-redux';
-import { Wrapper, Container, ImagemContainer, Textos } from './QuemSou.style';
+import { Wrapper, Container, ImagemContainer, Textos, SpaceShip } from './QuemSou.style';
 import { Link } from 'react-router-dom';
 import IntroBackground from '../../Components/IntroBackground/IntroBackground';
 
@@ -10,12 +10,25 @@ function QuemSou(props) {
     props.dispatch(PageActions.AlterarPagina(1));
   });
 
+  const [AmountFire] = React.useState(5);
+
+  const [Fire] = React.useState(() => {
+    let chamas = [];
+    for (let i = 0; i < AmountFire; i++) {
+      chamas.push(<div className="chama" key={Math.random()} />);
+    }
+    return chamas;
+  });
+
   return (
     <Wrapper>
       <IntroBackground />
       <Container>
         <ImagemContainer>
-          <img src="https://site-do-diego.s3-sa-east-1.amazonaws.com/rocket.png" alt="Foguetinho" />
+          <SpaceShip>
+            <img src="https://site-do-diego.s3-sa-east-1.amazonaws.com/rocket.png" alt="Foguetinho" />
+            {Fire.map((chama) => chama)}
+          </SpaceShip>
         </ImagemContainer>
         <Textos>
           <h1>Quem Sou??</h1>
@@ -42,7 +55,6 @@ function QuemSou(props) {
             Com a necessidade de um backend mais proprio para a ferramenta, conheci o <span>NodeJs</span>, onde conheci
             e estudei com bem mais afinco a <span>Stack Javascript</span> que estudo até atualmente.
           </p>
-
           <p>
             Para conhecer meus trabalhos, você pode
             <Link to="/portifolio">
@@ -54,7 +66,6 @@ function QuemSou(props) {
             </Link>{' '}
             para ser enviado a minha página de contato!
           </p>
-
           <p style={{ textAlign: 'center' }}>Te aguardo lá!!</p>
         </Textos>
       </Container>
--- src/Pages/QuemSou/QuemSou.style.js
+++ src/Pages/QuemSou/QuemSou.style.js
@@ -33,24 +33,48 @@ export const RocketMoviment = keyframes`
   }
 `;
 
-export const ImagemContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  img {
-    animation: ${RocketMoviment} 0.2s linear infinite;
-    margin: 30px;
-  }
-`;
-
 export const Container = styled.div`
   display: grid;
   grid-template-columns: 1fr 2fr;
   align-items: center;
 
-  @media (max-width: 1000px) {
+  @media (max-width: 700px) {
     grid-template-columns: 1fr;
+    grid-template-rows: 200px 2fr;
+    align-items: flex-start;
+  }
+`;
+
+export const ImagemContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  animation: ${RocketMoviment} 0.2s linear infinite;
+`;
+
+export const SpaceShip = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: red;
+
+  @media (max-width: 700px) {
+    height: 100px;
+  }
+
+  img {
+    margin: 30px;
+    z-index: 1;
+    position: absolute;
+
+    @media (max-width: 600px) {
+      transform: scale(0.8);
+    }
+
+    @media (max-width: 350px) {
+      transform: scale(0.5);
+    }
   }
 `;
 
@@ -61,6 +85,18 @@ export const Textos = styled.div`
     font-size: 3em;
     letter-spacing: 2px;
     margin-bottom: 30px;
+
+    @media (max-width: 1200px) {
+      font-size: 2em;
+    }
+
+    @media (max-width: 600px) {
+      font-size: 1em;
+    }
+
+    @media (max-width: 350px) {
+      font-size: 0.8em;
+    }
   }
 
   p {
@@ -68,7 +104,29 @@ export const Textos = styled.div`
     font-size: 1.4em;
     letter-spacing: 0.12em;
     line-height: 1.8em;
-    margin-bottom: 30px;
+    margin-bottom: 2.6em;
+    text-align: justify;
+
+    @media (max-width: 1200px) {
+      font-size: 1.2em;
+      margin-bottom: 2em;
+    }
+
+    @media (max-width: 1200px) {
+      font-size: 0.9em;
+      margin-bottom: 0.9em;
+      margin-bottom: 1.5em;
+    }
+
+    @media (max-width: 720px) {
+      font-size: 0.7em;
+      margin-bottom: 1.4em;
+    }
+
+    @media (max-width: 350px) {
+      font-size: 0.5em;
+      margin-bottom: 1em;
+    }
   }
 
   span {
